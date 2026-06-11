47f2b938c86fb8c6ef0a7e741ebdf46147cab0d7
SYED MOHMED FAIZ ROZDAR <130144532+SyedMohmedFaizRozdar@users.noreply.github.com>
2026-06-11 23:37:02 +0530

    Add: buy-and-sell-crypto - submission-0

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-0.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int maxProfit(int[] prices) {
+        int maxProfit = 0;
+        int minPrice= prices[0];
+
+        for(int i=0;i<prices.length;i++){
+            maxProfit = Math.max(maxProfit, prices[i]-minPrice);
+            minPrice= Math.min(minPrice, prices[i]);
+        }
+
+        return maxProfit;
+    }
+}
